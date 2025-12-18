02d72e7e98eb2a96339fdc491d870ba1be78c285
Santos <ssantyxs@gmail.com>
2025-12-18 20:13:43 +0100

    Simplifies time display in /check command and adds infinite fly time.

--- src/main/java/pwf/xenova/commands/CheckCommand.java
+++ src/main/java/pwf/xenova/commands/CheckCommand.java
@@ -28,17 +28,14 @@ public record CheckCommand(PowerFly plugin) implements CommandExecutor {
         OfflinePlayer target;
 
         if (args.length < 1) {
-
             if (sender instanceof Player player) {
                 target = player;
             } else {
                 sendWithPrefix(sender, plugin.getMessageString("no-player-specified", "&cYou must specify a player name."));
                 return true;
             }
-
         } else {
             target = Bukkit.getOfflinePlayer(args[0]);
-
             if (!target.hasPlayedBefore() && !target.isOnline()) {
                 sendWithPrefix(sender, plugin.getMessageString("player-not-found", "&cPlayer not found or offline."));
                 return true;
@@ -46,40 +43,38 @@ public record CheckCommand(PowerFly plugin) implements CommandExecutor {
         }
 
         UUID uuid = target.getUniqueId();
-        String playerName = Objects.requireNonNullElse(
-                target.getName(),
-                args.length > 0 ? args[0] : "Unknown"
-        );
+        String playerName = Objects.requireNonNullElse(target.getName(), args.length > 0 ? args[0] : "Unknown");
 
         int flySeconds = plugin.getFlyTimeManager().getRemainingFlyTime(uuid);
+        String flyTimeDisplay;
+
+        if (flySeconds < 0) {
+            flyTimeDisplay = "∞";
+        } else {
+            int flyMinutes = flySeconds / 60;
+            int flyRemSeconds = flySeconds % 60;
+            flyTimeDisplay = flyMinutes + "m " + flyRemSeconds + "s";
+        }
+
         int cooldownSeconds = plugin.getCooldownFlyManager().getRemainingCooldownSeconds(uuid);
-
-        int flyMinutes = flySeconds / 60;
-        int flyRemSeconds = flySeconds % 60;
-
         int cooldownMinutes = cooldownSeconds / 60;
         int cooldownRemSeconds = cooldownSeconds % 60;
+        String cooldownDisplay = cooldownMinutes + "m " + cooldownRemSeconds + "s";
 
         String raw = plugin.getMessages().getString("check-info",
                 """
                 &8&m-&r &b{player} Info &8&m-
                 
-                &bTime fly: &7{fly_minutes}m {fly_seconds}s
-                &bCooldown: &7{cooldown_minutes}m {cooldown_seconds}s"""
+                &bTime fly: &7{fly_time}
+                &bCooldown: &7{cooldown_time}"""
         );
 
         raw = raw.replace("{player}", playerName)
-                .replace("{fly_minutes}", String.valueOf(flyMinutes))
-                .replace("{fly_seconds}", String.valueOf(flyRemSeconds))
-                .replace("{cooldown_minutes}", String.valueOf(cooldownMinutes))
-                .replace("{cooldown_seconds}", String.valueOf(cooldownRemSeconds));
+                .replace("{fly_time}", flyTimeDisplay)
+                .replace("{cooldown_time}", cooldownDisplay);
 
-        send(sender, raw);
+        sender.sendMessage(MessageFormat.parseMessage(raw));
         return true;
-    }
-
-    private void send(CommandSender sender, String message) {
-        sender.sendMessage(MessageFormat.parseMessage(message));
     }
 
     private void sendWithPrefix(CommandSender sender, String message) {
