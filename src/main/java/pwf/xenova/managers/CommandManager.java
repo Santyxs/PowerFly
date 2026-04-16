a9ab9560131a6a39ffd34905e4a915578fe65a60
Santos <ssantyxs@gmail.com>
2026-04-16 18:42:46 +0200

    Refactors command tab completer, bumps config, and fixes translations.

--- src/main/java/pwf/xenova/managers/CommandManager.java
+++ src/main/java/pwf/xenova/managers/CommandManager.java
@@ -9,6 +9,19 @@ import java.util.*;
 
 public class CommandManager {
 
+    private static List<String> getPlayerOptions() {
+        List<String> options = new ArrayList<>();
+        options.add("all");
+        Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
+        return options;
+    }
+
+    private static List<String> getOnlinePlayers() {
+        List<String> options = new ArrayList<>();
+        Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
+        return options;
+    }
+
     public static void registerCommands(PowerFly plugin) {
         if (!(plugin instanceof PowerFly powerFly)) {
             throw new IllegalArgumentException("Plugin must be an instance of PowerFly");
@@ -28,75 +41,57 @@ public class CommandManager {
 
         Objects.requireNonNull(plugin.getCommand("fly")).setExecutor(flyCommand);
         Objects.requireNonNull(plugin.getCommand("buyflytime")).setExecutor(buyFlyTimeCommand);
+
         Objects.requireNonNull(plugin.getCommand("powerfly")).setExecutor((sender, command, label, args) -> {
             if (args.length < 1) return false;
 
             String[] subArgs = Arrays.copyOfRange(args, 1, args.length);
 
             return switch (args[0].toLowerCase()) {
-                case "help" -> helpCommand.onCommand(sender, command, label, subArgs);
-                case "reload" -> reloadCommand.onCommand(sender, command, label, subArgs);
-                case "fly" -> flyCommand.onCommand(sender, command, label, subArgs);
-                case "check" -> checkCommand.onCommand(sender, command, label, subArgs);
-                case "addflytime" -> addFlyTimeCommand.onCommand(sender, command, label, subArgs);
-                case "delflytime" -> delFlyTimeCommand.onCommand(sender, command, label, subArgs);
-                case "buyflytime" -> buyFlyTimeCommand.onCommand(sender, command, label, subArgs);
-                case "reset" -> resetCommand.onCommand(sender, command, label, subArgs);
-                case "setflytime" -> setFlyTimeCommand.onCommand(sender, command, label, subArgs);
+                case "help"        -> helpCommand.onCommand(sender, command, label, subArgs);
+                case "reload"      -> reloadCommand.onCommand(sender, command, label, subArgs);
+                case "fly"         -> flyCommand.onCommand(sender, command, label, subArgs);
+                case "check"       -> checkCommand.onCommand(sender, command, label, subArgs);
+                case "addflytime"  -> addFlyTimeCommand.onCommand(sender, command, label, subArgs);
+                case "delflytime"  -> delFlyTimeCommand.onCommand(sender, command, label, subArgs);
+                case "buyflytime"  -> buyFlyTimeCommand.onCommand(sender, command, label, subArgs);
+                case "reset"       -> resetCommand.onCommand(sender, command, label, subArgs);
+                case "setflytime"  -> setFlyTimeCommand.onCommand(sender, command, label, subArgs);
                 case "setcooldown" -> setCooldownCommand.onCommand(sender, command, label, subArgs);
-                default -> false;
+                default            -> false;
             };
         });
 
         Objects.requireNonNull(plugin.getCommand("powerfly")).setTabCompleter((sender, command, label, args) -> {
             if (args.length == 1) {
-                List<String> subcommands = List.of(
-                        "help", "reload", "fly", "check", "addflytime", "delflytime", "buyflytime", "reset", "setflytime", "setcooldown"
-                );
+                List<String> subcommands = List.of("help", "reload", "fly", "check", "addflytime", "delflytime", "buyflytime", "reset", "setflytime", "setcooldown");
                 return StringUtil.copyPartialMatches(args[0], subcommands, new ArrayList<>());
             }
 
             if (args.length == 2) {
-                String subcommand = args[0].toLowerCase();
-
-                switch (subcommand) {
-                    case "fly", "addflytime", "delflytime", "setflytime", "setcooldown" -> {
-                        List<String> options = new ArrayList<>();
-                        options.add("all");
-                        Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
-                        return StringUtil.copyPartialMatches(args[1], options, new ArrayList<>());
-                    }
-                    case "reset" -> {
-                        return StringUtil.copyPartialMatches(args[1], List.of("cooldown", "flytime"), new ArrayList<>());
-                    }
-                    case "check" -> {
-                        List<String> options = new ArrayList<>();
-                        Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
-                        return StringUtil.copyPartialMatches(args[1], options, new ArrayList<>());
-                    }
-                    case "buyflytime" -> {
-                        return List.of("<seconds>");
-                    }
-                }
+                return switch (args[0].toLowerCase()) {
+                    case "fly", "addflytime", "delflytime", "setflytime", "setcooldown" ->
+                            StringUtil.copyPartialMatches(args[1], getPlayerOptions(), new ArrayList<>());
+                    case "check" ->
+                            StringUtil.copyPartialMatches(args[1], getOnlinePlayers(), new ArrayList<>());
+                    case "reset" ->
+                            StringUtil.copyPartialMatches(args[1], List.of("cooldown", "flytime"), new ArrayList<>());
+                    case "buyflytime" ->
+                            List.of("<seconds>");
+                    default -> new ArrayList<>();
+                };
             }
 
             if (args.length == 3) {
-                String subcommand = args[0].toLowerCase();
-
-                switch (subcommand) {
-                    case "fly" -> {
-                        return StringUtil.copyPartialMatches(args[2], List.of("on", "off"), new ArrayList<>());
-                    }
-                    case "addflytime", "delflytime", "setflytime", "setcooldown" -> {
-                        return StringUtil.copyPartialMatches(args[2], List.of("60", "300", "600", "3600", "-1"), new ArrayList<>());
-                    }
-                    case "reset" -> {
-                        List<String> options = new ArrayList<>();
-                        options.add("all");
-                        Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
-                        return StringUtil.copyPartialMatches(args[2], options, new ArrayList<>());
-                    }
-                }
+                return switch (args[0].toLowerCase()) {
+                    case "fly" ->
+                            StringUtil.copyPartialMatches(args[2], List.of("on", "off"), new ArrayList<>());
+                    case "addflytime", "delflytime", "setflytime", "setcooldown" ->
+                            StringUtil.copyPartialMatches(args[2], List.of("60", "300", "600", "3600", "-1"), new ArrayList<>());
+                    case "reset" ->
+                            StringUtil.copyPartialMatches(args[2], getPlayerOptions(), new ArrayList<>());
+                    default -> new ArrayList<>();
+                };
             }
 
             return new ArrayList<>();
@@ -104,23 +99,16 @@ public class CommandManager {
 
         Objects.requireNonNull(plugin.getCommand("fly")).setTabCompleter((sender, command, label, args) -> {
             if (args.length == 1) {
-                List<String> options = new ArrayList<>();
-                options.add("all");
-                Bukkit.getOnlinePlayers().forEach(p -> options.add(p.getName()));
-                return StringUtil.copyPartialMatches(args[0], options, new ArrayList<>());
+                return StringUtil.copyPartialMatches(args[0], getPlayerOptions(), new ArrayList<>());
             }
-
             if (args.length == 2) {
                 return StringUtil.copyPartialMatches(args[1], List.of("on", "off"), new ArrayList<>());
             }
-
             return new ArrayList<>();
         });
 
         Objects.requireNonNull(plugin.getCommand("buyflytime")).setTabCompleter((sender, command, label, args) -> {
-            if (args.length == 1) {
-                return List.of("<seconds>");
-            }
+            if (args.length == 1) return List.of("<seconds>");
             return new ArrayList<>();
         });
     }
